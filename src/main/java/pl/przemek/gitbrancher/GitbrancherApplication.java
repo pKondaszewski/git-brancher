fe998bd6e4e4f33b2d38cd9c094c95dfe8566c14
pKondaszewski <kondaszewskiprz@gmail.com>
2024-02-09 00:11:01 +0100

    Init commit

---
+++ src/main/java/pl/przemek/gitbrancher/GitbrancherApplication.java
@@ -0,0 +1,13 @@
+package pl.przemek.gitbrancher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbrancherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbrancherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/przemek/gitbrancher/GitbrancherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.przemek.gitbrancher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbrancherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
